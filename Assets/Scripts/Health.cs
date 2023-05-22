9fec19a0da9ff584f8f095137c6014ac7bb66526
Bushra Ashraf <bushra.ashraf284@gmail.com>
2023-05-22 19:24:22 +0500

    Setting Up Player
    -> Setup Player Controller and model
    -> Creating Health System

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int currentHealth;
+    public int MaxHealth;
+    [SerializeField] Slider healthSlider;
+
+    void Start()
+    {
+        
+    }
+
+    public void SetHealth(int health)
+    {
+        currentHealth = MaxHealth = health;
+        UpdateHealthBar();
+    }
+
+    public IEnumerator TakeDamageDelayed(int amount, int delay)
+    {
+        yield return new WaitForSeconds(delay);
+        currentHealth = Mathf.Max(currentHealth - amount, 0);
+        if (currentHealth == 0) { 
+            //dIE
+        }
+        UpdateHealthBar();
+    }
+
+    public void UpdateHealthBar()
+    {
+        healthSlider.value = (float)currentHealth / (float) MaxHealth;
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth = Mathf.Min(currentHealth + amount, MaxHealth);
+        UpdateHealthBar();
+    }
+    
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector3 movementVector;
+    public float speed;
+    private Animator animator;
+    private Rigidbody rb;
+    private Health health;
+    
+
+
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody>();
+        health = GetComponent<Health>();
+        speed = 1.5f;
+    }
+
+    private void Update()
+    {
+        CalculateMovement();
+        if(movementVector != Vector3.zero) {
+            SetRotation();
+        }
+        animator.SetBool("Walking", movementVector != Vector3.zero);
+
+    }
+
+    private void CalculateMovement()
+    {
+        movementVector = new Vector3 (Input.GetAxis("Horizontal"),rb.velocity.y, Input.GetAxis("Vertical"));
+        rb.velocity = new Vector3(movementVector.x * speed, movementVector.y, movementVector.z * speed);
+
+    }
+
+    private void SetRotation()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movementVector), 0.1f);
+    }
+}
